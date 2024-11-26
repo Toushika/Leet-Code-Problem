d6a6095257353795a6ee67be0430418862befea1
toushika111 <toushika.islam@s2021.tu-chemnitz.de>
2024-11-27 00:08:45 +0100

    modify code

--- src/problem-1-1/src/Main.java
+++ src/problem-1-1/src/Main.java
@@ -1,24 +1,24 @@
 import java.util.HashMap;
 import java.util.Map;
 
-class Solution {
+class Main {
 
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> hashMap = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             if (hashMap.containsKey(complement)) {
-              return new int[]{hashMap.get(complement), i};
+                return new int[]{hashMap.get(complement), i};
             } else {
                 hashMap.put(nums[i], i);
             }
         }
 
         return new int[]{};
-}
+    }
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        Main solution = new Main();
         int[] nums = {3,2,4};
         int target = 6;
         int[] result = solution.twoSum(nums, target);
