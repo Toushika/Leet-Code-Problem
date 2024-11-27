605e24a5c2d5e4bdf81c96e446a253e4397d81a1
toushika111 <toushika.islam@s2021.tu-chemnitz.de>
2024-11-27 16:16:55 +0100

    problem-2-9 has been added

--- src/problem-2-9/src/Main.java
+++ src/problem-2-9/src/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+class Solution {
+    public boolean isPalindrome(int x) {
+        String numString = String.valueOf(x);
+        int left = 0;
+        int right = numString.length()-1;
+        while(left < right) {
+            if(numString.charAt(left) !=numString.charAt(right)) {
+                return false;
+            } else {
+                left++;
+                right--;
+            }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+            }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        boolean result = solution.isPalindrome(x);
+        System.out.println("Result: " + result);
     }
 }
